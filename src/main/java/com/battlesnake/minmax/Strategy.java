89a52cfa6a85887c254db2af657240cf6611d8ee
colin <colinsullivan203@gmail.com>
2022-01-01 19:08:49 -0500

    switched to 400ms time limit

--- src/main/java/com/battlesnake/minmax/Strategy.java
+++ src/main/java/com/battlesnake/minmax/Strategy.java
@@ -10,7 +10,7 @@ import java.util.stream.Stream;
 
 public class Strategy {
 
-    private static final long CUTOFF_TIME_ELAPSED = 300;
+    private static final long CUTOFF_TIME_ELAPSED = 400;
     private static final int MAX_DEPTH = 100;
     private static final Logger LOG = LoggerFactory.getLogger(Strategy.class);
 
